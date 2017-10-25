bd3dad808d8a638def3f321d349dc8f70ec55945
bbworld <vw1212@pleasantonusd.net>
2017-10-25 13:57:47 -0700

    Added files from old projects and suggested a more inspector based ui

--- video.py
+++ video.py
@@ -6,13 +6,12 @@ import player_api
 
 class Video:
     def __init__(self, hour: int, minute: int, second: int, name: str, flags: list):
-        # self.thread = threading.Thread(name=name, target=self.play_at_time())
+        self.thread = threading.Thread(name=self.filename, target=self.play_at_time())
         self.hour = hour
         self.minute = minute
         self.second = second
         self.filename = name
         self.flags = flags
-        print(self.hour, self.minute, self.second)
 
     def set_play_time(self, hour: int, minute: int, second: int):
         self.hour = hour
@@ -26,13 +25,12 @@ class Video:
         a) the time is after current time
         b) the video has NOT started playing yet
         """
-        # self.thread.start()
-        self.play_at_time()
+        self.thread.start()
 
     def play_at_time(self):
         while datetime.datetime.now().time() < datetime.time(self.hour, self.minute, self.second):
             time.sleep(0.05)
         self.play()
 
-    def play(self, player=player_api.PLAYER_VLC):
+    def play(self, player=player_api.PLAYER_CVLC):
         player_api.play(player, self.filename, self.flags)
