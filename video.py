8aecfef22a560e317567a2fd3ac2d32fde61fa11
Kalyan Sriram <coder.kalyan@gmail.com>
2017-10-30 21:08:55 -0700

    (Kalyan) Changed QTableWidget to QTableView. Also cleaned up a little.

--- video.py
+++ video.py
@@ -10,8 +10,7 @@ class Video:
         self.minute = minute
         self.second = second
         self.filename = name
-        self.flags = flags        
-        self.thread = threading.Thread(name=self.filename, target=self.play_at_time())
+        self.flags = flags
 
     def set_play_time(self, hour: int, minute: int, second: int):
         self.hour = hour
@@ -25,7 +24,7 @@ class Video:
         a) the time is after current time
         b) the video has NOT started playing yet
         """
-        self.thread.start()
+        # self.thread.start()
 
     def play_at_time(self):
         print(self.hour, self.minute, self.second)
@@ -35,6 +34,10 @@ class Video:
         while datetime.datetime.now().time() <= datetime.time(self.hour, self.minute, self.second):
             time.sleep(0.05)
         self.play()
+
+    @staticmethod
+    def num_columns():
+        return 3
 
     def play(self, player=player_api.PLAYER_VLC):
         player_api.play(player, self.filename, self.flags)
