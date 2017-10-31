b0a0ac3e370598f4befbe462e4094c1b303e2758
bbworld <vw1212@pleasantonusd.net>
2017-10-30 19:07:30 -0700

    Added setup.py to prepare for building executable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Video Scheduler by KVK',
+    version='1.4  Delicious Apple',
+    packages=[''],
+    url='',
+    license='',
+    author='bbworld',
+    author_email='vw1212@pleasantonusd.net',
+    description='Table-based video scheduler'
+)
